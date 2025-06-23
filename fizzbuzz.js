58f0b9c3e283b06d7d6e600b0fb9dc8490ad95d7
Halrox <jeromeDeledda@gmail.com>
2025-06-23 06:45:00 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,26 +1,24 @@
-    //Assuming that 'FizzBuzz' (for multiples of both 3 and 5) is simply the concatenation of 'Fizz' and 'Buzz'
-    
-        //Readable Version
-        function fizzBuzz(number)
+//Assuming that 'FizzBuzz' (for multiples of both 3 and 5) is simply the concatenation of 'Fizz' and 'Buzz'
+
+    //Readable Version
+    function fizzBuzz(number) 
+    {
+        const results = [];
+        for (let i = 1; i <= number; i++) 
         {
-            const results = [];
+            let value = '';
+            if (i % 3 === 0) value += 'Fizz';
+            if (i % 5 === 0) value += 'Buzz';
+            results.push(value || i);
+        }
+        return results;
+    }
+
+    // Compact version 
+    function fizzBuzzCompact(number)
+    {
             for (let i = 1; i <= number; i++) 
-            {
-                let value = '';
+            results.push(((i % 3 === 0) ? 'Fizz' : '') + ((i % 5 === 0) ? 'Buzz' : '') || i);
 
-                value += (i % 3 === 0) ? 'Fizz' : '';
-                value += (i % 5 === 0) ? 'Buzz' : '';
-
-                results.push(value || i);
-            }
-            return results;
-        }
-
-        // Compact version 
-        function fizzBuzzCompact(number)
-        {
-             for (let i = 1; i <= number; i++) 
-                results.push(((i % 3 === 0) ? 'Fizz' : '') + ((i % 5 === 0) ? 'Buzz' : '') || i);
-
-            return results;
-        }
+        return results;
+    }
